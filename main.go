88ee29d006d34d8c10975a959e47c5cb52278a29
Dan Anstis <dan@bsod.co.nz>
2017-03-11 13:29:28 +1000

    Update

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+}
