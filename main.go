c9b37d014aa8cf95123ee0f17669f3dec2c96285
Dan Anstis <dan@bsod.co.nz>
2017-08-19 10:55:41 +1000

    remove extra comments

--- main.go
+++ main.go
@@ -66,18 +66,9 @@ type localserver struct {
 
 // RootHandler returns the default page.
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	// w.WriteHeader(http.StatusOK)
 	http.ServeFile(w, r, path.Join("templates", "index.html"))
 }
 
 func tokenHandler(w http.ResponseWriter, r *http.Request) {
-	// tmplt := template.New("promptCredentials.html")
-	// tmplt, err := tmplt.ParseFiles(path.Join("templates", "settings", "promptCredentials.html"))
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusOK)
-	// 	return
-	// }
-	//w.WriteHeader(http.StatusOK)
-	// tmplt.Execute(w, "something")
 	http.ServeFile(w, r, path.Join("templates", "settings", "promptCredentials.html"))
 }
