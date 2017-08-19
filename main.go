4a03b19c84dd4abe60336ea4612c32631a294e9b
Dan Anstis <dan@bsod.co.nz>
2017-08-19 10:41:45 +1000

    Add index page and static folder

--- main.go
+++ main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"net/http"
+
+	"time"
+
+	"path"
 
 	"github.com/danstis/Plex-Sync/plex"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
 )
 
 func main() {
 
+	r := mux.NewRouter()
+	r.HandleFunc("/", rootHandler)
+	s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
+	r.PathPrefix("/static/").Handler(s)
+	http.Handle("/", r)
+	r.HandleFunc("/settings/token", tokenHandler)
+	http.Handle("/settings/token", r)
+
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	go http.ListenAndServe(":80", loggedRouter)
+	log.Println("Started webserver http://localhost")
+
 	cp := plex.CredPrompter{}
-	r := plex.TokenRequester{}
-	token, err := plex.Token(cp, r)
+	tr := plex.TokenRequester{}
+	token, err := plex.Token(cp, tr)
 	if err != nil {
 		log.Printf("Error: %v", err)
 	}
@@ -30,10 +51,33 @@ func main() {
 	}
 
 	log.Println("Local server details:", localserver)
+
+	for {
+		log.Println("Waiting")
+		time.Sleep(60 * time.Second)
+	}
 }
 
 type localserver struct {
 	name     string
 	hostname string
 	port     int
+}
+
+// RootHandler returns the default page.
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	// w.WriteHeader(http.StatusOK)
+	http.ServeFile(w, r, path.Join("templates", "index.html"))
+}
+
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+	// tmplt := template.New("promptCredentials.html")
+	// tmplt, err := tmplt.ParseFiles(path.Join("templates", "settings", "promptCredentials.html"))
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusOK)
+	// 	return
+	// }
+	//w.WriteHeader(http.StatusOK)
+	// tmplt.Execute(w, "something")
+	http.ServeFile(w, r, path.Join("templates", "settings", "promptCredentials.html"))
 }
