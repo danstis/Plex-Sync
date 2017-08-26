5d20e0bd09da8d1b5349b0e55ea2ffaa366b14b8
Dan Anstis <dan@bsod.co.nz>
2017-08-26 11:34:27 +1000

    Update test to match updated fuction

--- plex/server_test.go
+++ plex/server_test.go
@@ -8,19 +8,18 @@ func TestCreateURI(t *testing.T) {
 	type args struct {
 		server Host
 		path   string
-		token  string
 	}
 	tests := []struct {
 		name string
 		args args
 		want string
 	}{
-		{"Test SSL", args{server: Host{Name: "Test SSL", Hostname: "localhost", Port: 2121, Ssl: true}, path: "test", token: "123456"}, "https://localhost:2121/test?X-Plex-Token=123456"},
-		{"Test HTTP", args{server: Host{Name: "Test HTTP", Hostname: "servername", Port: 1515, Ssl: false}, path: "new", token: "789456"}, "http://servername:1515/new?X-Plex-Token=789456"},
+		{"Test SSL", args{server: Host{Name: "Test SSL", Hostname: "localhost", Port: 2121, Ssl: true}, path: "test"}, "https://localhost:2121/test"},
+		{"Test HTTP", args{server: Host{Name: "Test HTTP", Hostname: "servername", Port: 1515, Ssl: false}, path: "new"}, "http://servername:1515/new"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CreateURI(tt.args.server, tt.args.path, tt.args.token); got != tt.want {
+			if got := CreateURI(tt.args.server, tt.args.path); got != tt.want {
 				t.Errorf("CreateURI() = %v, want %v", got, tt.want)
 			}
 		})
