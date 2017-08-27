1d7bea31b7327d87042ab94f059feb59a27d9f1e
Dan Anstis <dan@bsod.co.nz>
2017-08-27 16:13:14 +1000

    Add scrobbling of watched episodes

--- plex/server.go
+++ plex/server.go
@@ -1,11 +1,13 @@
 package plex
 
 import (
+	"bufio"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 // Host defines the data to be stored for server objects
@@ -17,6 +19,10 @@ type Host struct {
 	Token     string
 	TvSection int
 }
+
+var (
+	tvShowFile = "tvshows.txt"
+)
 
 // CreateURI assembles the URI for an API request
 func CreateURI(server Host, path string) string {
@@ -59,29 +65,159 @@ func SearchShow(server Host, title string) (Show, error) {
 	return Show{}, fmt.Errorf("no show found matching name %q", title)
 }
 
-//SR contains search results
+// SR contains search results
 type SR struct {
 	XMLName     xml.Name `xml:"MediaContainer"`
 	Directories []Show   `xml:"Directory"`
 }
 
-//Show defines the structure of a Plex TV Show
+// Show defines the structure of a Plex TV Show
 type Show struct {
 	ID           int    `xml:"ratingKey,attr"`
 	Name         string `xml:"title,attr"`
 	EpisodeCount int    `xml:"leafCount,attr"`
 }
 
-//SyncWatchedTv synchronises the watched TV Shows
-func SyncWatchedTv(source, destination Host) {
+// ER contais episode results
+type ER struct {
+	XMLName xml.Name  `xml:"MediaContainer"`
+	Video   []Episode `xml:"Video"`
+}
+
+// Episode defines the structure of a Plex TV Episode
+type Episode struct {
+	ID          int    `xml:"ratingKey,attr"`
+	Name        string `xml:"title,attr"`
+	Episode     int    `xml:"index,attr"`
+	Season      int    `xml:"parentIndex,attr"`
+	ViewCount   int    `xml:"viewCount,attr"`
+	LastWatched int    `xml:"lastViewedAt,attr"`
+}
+
+// SyncWatchedTv synchronises the watched TV Shows
+func SyncWatchedTv(source, destination Host) error {
 	log.Printf("Syncing watched Tv Shows from %q to %q", source.Name, destination.Name)
 
-	// Return all shows on the source server
+	// Return all selected shows
+	ss, err := selectedShows()
+	if err != nil {
+		return err
+	}
 
-	// For each show on the source server, enumerate all episodes
+	// For each show, enumerate all source and destination episodes
+	for _, s := range ss {
+		log.Printf("Processing show %q", s)
+		srcShow, err := SearchShow(source, s)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		sEps, err := allEpisodes(source, srcShow.ID)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		destShow, err := SearchShow(destination, s)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		dEps, err := allEpisodes(destination, destShow.ID)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		for _, e := range sEps {
+			// If the local show is marked as watched check if the remote episode is watched
+			log.Printf("- Checking %v - Season %v, Episode %v", srcShow.Name, e.Season, e.Episode)
+			destEp, err := findEpisode(dEps, e.Season, e.Episode)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if e.ViewCount > 0 && destEp.ViewCount < 1 {
+				// Scrobble the episode on the remote server
+				err = scrobble(destination, destEp.ID)
+				if err != nil {
+					log.Printf("failed to scrobble episode. Error: %v", err)
+					continue
+				}
+				log.Printf("* Scrobbled on %q", destination.Name)
+			} else {
+				log.Println("- Episode not yet watched, skipping...")
+			}
+		}
+	}
+	return nil
+}
 
-	// If the local show is marked as watched check if the remote episode is watched
+// selectedShows returns the selected tv shows from the tvShowsFile
+func selectedShows() ([]string, error) {
+	file, err := os.Open(tvShowFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open tvshows file %q", tvShowFile)
+	}
 
-	// Scrobble the episode on the remote server
+	defer file.Close()
+	var lines []string
 
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}
+
+// allEpisodes returns all child episodes of a tv show regardless of the season they belong to
+func allEpisodes(server Host, sID int) ([]Episode, error) {
+	uri := CreateURI(server, fmt.Sprintf("library/metadata/%v/allLeaves", sID))
+	resp, err := apiRequest("GET", uri, server.Token, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected HTTP Response %q", resp.Status)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response %v", err)
+	}
+
+	results := ER{}
+
+	err = xml.Unmarshal(body, &results)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing xml response: %v", err)
+	}
+
+	return results.Video, nil
+}
+
+// findEpisode returns a single episode from a slice of Episodes based on the season and episode number
+func findEpisode(eps []Episode, s, e int) (Episode, error) {
+	for _, i := range eps {
+		if i.Season == s && i.Episode == e {
+			return i, nil
+		}
+	}
+
+	return Episode{}, fmt.Errorf("could not find episode in array")
+}
+
+// scrobble marks an episode as watched
+func scrobble(server Host, eID int) error {
+	uri := CreateURI(server, fmt.Sprintf(":/scrobble?key=%v&identifier=com.plexapp.plugins.library", eID))
+	resp, err := apiRequest("GET", uri, server.Token, nil)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected HTTP Response %q", resp.Status)
+	}
+
+	return nil
 }
