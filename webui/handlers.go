088fedb5e4f35713ba055552a2b10cec9965082e
Dan Anstis <dan@bsod.co.nz>
2017-09-17 14:13:53 +1000

    Add logs to webinterface

--- webui/handlers.go
+++ webui/handlers.go
@@ -2,8 +2,12 @@ package webui
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"path"
+
+	"github.com/gorilla/mux"
 
 	"github.com/danstis/Plex-Sync/plex"
 )
@@ -19,6 +23,32 @@ func settingsHandler(w http.ResponseWriter, r *http.Request) {
 
 func tokenHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, path.Join("webui", "templates", "settings", "promptCredentials.html"))
+}
+
+func logsHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, path.Join("webui", "templates", "logs.html"))
+}
+
+func generalLogHeadHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	file := vars["logfile"]
+	fi, err := os.Stat(path.Join("logs", file))
+	if err != nil {
+		log.Println("Failed to find log file")
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Length", fmt.Sprintf("%v", fi.Size()))
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Accept-Ranges", "bytes")
+	w.WriteHeader(http.StatusOK)
+}
+
+func generalLogHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	file := vars["logfile"]
+	http.ServeFile(w, r, path.Join("logs", file))
 }
 
 func tokenRequestHandler(w http.ResponseWriter, r *http.Request) {
