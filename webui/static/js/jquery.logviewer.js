1fd3a747625ba982237089463594ab344829f730
Dan Anstis <dan@bsod.co.nz>
2017-10-14 12:00:40 +1000

    Undo changes to resolve issues

--- webui/static/js/jquery.logviewer.js
+++ webui/static/js/jquery.logviewer.js
@@ -1,23 +1,20 @@
 (function(jQuery) {
     var logViewer = (function(options) {
-        (function cleanLVtags(html) {
-            if (typeof html === "string") {
-                return html
-                    .replace(/&/g, "&amp;")
-                    .replace(/</g, "&lt;")
-                    .replace(/>/g, "&gt;");
-            } else {
-                return html;
-            }
-        },
-            function setMyTimeOut() {
-                if (logViewer.timeoutID > 0) {
-                    window.clearTimeout(logViewer.timeoutID);
+        (doLVHead = function(id) {
+            jQuery.ajax({
+                type: "HEAD",
+                url: logViewer.options.logUrl,
+                cache: false,
+                complete: function(xhr, textStatus) {
+                    if (textStatus === "success" || textStatus === "nocontent") {
+                        var newLength = parseInt(xhr.getResponseHeader("Content-Length"));
+                        checkLVLength(newLength);
+                    }
                 }
-                logViewer.timeoutID = window.setTimeout(doLVHead, logViewer.options.refreshtimeout);
-            },
-            function checkLVLength(newLength) {
-                if (logViewer.curLength !== newLength) {
+            });
+        }),
+            (checkLVLength = function(newLength) {
+                if (logViewer.curLength != newLength) {
                     if (logViewer.curLength > newLength) {
                         logViewer.curLength = 0;
                         jQuery("#" + logViewer.options.targetObjectID).append("\nReseting ... \n");
@@ -25,7 +22,7 @@
                     var getBytes = logViewer.curLength;
                     var readBytes = parseInt(logViewer.options.readBytes);
 
-                    if (logViewer.curLength === 0 && newLength > readBytes) {
+                    if (logViewer.curLength == 0 && newLength > readBytes) {
                         getBytes = newLength - readBytes;
                     } else if (logViewer.curLength > 0) {
                         getBytes--;
@@ -48,37 +45,38 @@
                 }
                 logViewer.curLength = newLength;
                 setMyTimeOut();
-            },
-            function doLVHead(id) {
-                jQuery.ajax({
-                    type: "HEAD",
-                    url: logViewer.options.logUrl,
-                    cache: false,
-                    complete: function(xhr, textStatus) {
-                        if (textStatus === "success" || textStatus === "nocontent") {
-                            var newLength = parseInt(xhr.getResponseHeader("Content-Length"));
-                            checkLVLength(newLength);
-                        }
-                    }
-                });
+            }),
+            (setMyTimeOut = function() {
+                if (logViewer.timeoutID > 0) {
+                    window.clearTimeout(logViewer.timeoutID);
+                }
+                logViewer.timeoutID = window.setTimeout(doLVHead, logViewer.options.refreshtimeout);
+            }),
+            (cleanLVtags = function(html) {
+                if (typeof html == "string") {
+                    return html
+                        .replace(/&/g, "&amp;")
+                        .replace(/</g, "&lt;")
+                        .replace(/>/g, "&gt;");
+                } else {
+                    return html;
+                }
             });
 
         return {
             init: function(options) {
-                if (!options) {
-                    var options = {};
-                }
-                if (options.logUrl === null) {
+                if (!options) var options = {};
+                if (options.logUrl == null) {
                     alert("Log URL missing");
                     return false;
                 }
-                if (options.refreshtimeout === null) {
+                if (options.refreshtimeout == null) {
                     options.refreshtimeout = "2000";
                 }
-                if (options.readBytes === null) {
+                if (options.readBytes == null) {
                     options.readBytes = 10000;
                 }
-                if (options.callback === null) {
+                if (options.callback == null) {
                     options.callback = function(data) {
                         return data;
                     };
