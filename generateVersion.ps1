a691117eaadef9ff15f61fb1f229d68cd0073d0c
Dan Anstis <dan@bsod.co.nz>
2017-10-20 21:24:24 +1000

    Add versioning script

---
+++ generateVersion.ps1
@@ -0,0 +1,8 @@
+# If GitVersion is not installed:
+#   choco install GitVersion.Portable
+
+$Version = Gitversion.exe | ConvertFrom-Json
+$VersionFile = ".\plex\version.go"
+
+Write-Host ("Updateing version to {0}" -f $Version.SemVer)
+(Get-Content $VersionFile) -replace '(?<=Version = ").*?(?=")', $Version.SemVer |  Out-File $VersionFile
