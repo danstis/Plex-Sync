c3bb8c56de3978c888f256cefe35346e1af10451
Dan Anstis <dan@bsod.co.nz>
2017-10-20 21:27:09 +1000

    Change output file to UTF8

--- generateVersion.ps1
+++ generateVersion.ps1
@@ -5,4 +5,4 @@ $Version = Gitversion.exe | ConvertFrom-Json
 $VersionFile = ".\plex\version.go"
 
 Write-Host ("Updateing version to {0}" -f $Version.SemVer)
-(Get-Content $VersionFile) -replace '(?<=Version = ").*?(?=")', $Version.SemVer |  Out-File $VersionFile
+(Get-Content $VersionFile) -replace '(?<=Version = ").*?(?=")', $Version.SemVer |  Out-File $VersionFile -Encoding utf8
