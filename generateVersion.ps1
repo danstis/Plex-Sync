577496bbd688783434e74218304eef353b0d01b5
Dan Anstis <dan@bsod.co.nz>
2017-10-22 13:25:22 +1000

    Update versioning script

--- generateVersion.ps1
+++ generateVersion.ps1
@@ -1,13 +1,5 @@
-param(
-	# Source Folder of the project
-	[string]
-	$SourceFolder = ".\"
-)
-
 $Version = Gitversion.exe | ConvertFrom-Json
-$VersionFile = "$SourceFolder\plex\version.go"
+$VersionFile = ".\plex\version.go"
 
 Write-Host ("Updateing version to {0}" -f $Version.SemVer)
-$Content = (Get-Content $VersionFile) -replace '(?<=Version = ").*?(?=")', $Version.SemVer 
-
-[IO.File]::WriteAllLines($VersionFile, $Content) # Write the file in UFT8 (no BOM)
+(Get-Content $VersionFile) -replace '(?<=Version = ").*?(?=")', $Version.SemVer | Set-Content -Path $VersionFile -Encoding UTF8
