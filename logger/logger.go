4e81de0190326917a284e4d5b79ee5a0ce9f7d12
Dan Anstis <dan@bsod.co.nz>
2017-12-01 12:54:48 +1000

    Move logging to new logger package

---
+++ logger/logger.go
@@ -0,0 +1,18 @@
+package logger
+
+import (
+	"io"
+	"os"
+
+	"github.com/danstis/Plex-Sync/config"
+	lumberjack "gopkg.in/natefinch/lumberjack.v2"
+)
+
+func CreateLogger(filename string) io.Writer {
+	return io.MultiWriter(&lumberjack.Logger{
+		Filename:   filename,
+		MaxSize:    config.Settings.General.MaxLogSize, // megabytes
+		MaxBackups: config.Settings.General.MaxLogCount,
+		MaxAge:     config.Settings.General.MaxLogAge, //days
+	}, os.Stdout)
+}
