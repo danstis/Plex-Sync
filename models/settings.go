7458244e9a010cab5c67b9740bad4912b663ac37
Dan Anstis <dan@bsod.co.nz>
2018-03-03 10:13:00 +1000

    Update settings model

--- models/settings.go
+++ models/settings.go
@@ -39,3 +39,32 @@ type Server struct {
 	Ssl      bool
 	Token    string
 }
+
+//GetSettings returns the settings record from the DB
+func GetSettings(db *gorm.DB) (Settings, error) {
+	var s Settings
+	if db.First(&s, 1).RecordNotFound() {
+		return defaults(), nil
+	}
+	return s, db.First(&s, 1).Error
+}
+
+func defaults() Settings {
+	s := Settings{
+		General: General{
+			SyncInterval:     600,
+			WebserverPort:    8085,
+			Logfile:          "logs/plex-sync.log",
+			WebserverLogfile: "logs/plex-sync-webserver.log",
+			MaxLogSize:       20,
+			MaxLogCount:      5,
+			MaxLogAge:        1,
+		},
+		LocalServer:  Server{},
+		RemoteServer: Server{},
+		Webui: Webui{
+			CacheLifetime: 5,
+		},
+	}
+	return s
+}
