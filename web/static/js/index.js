ce4b4b8c13f02aef11adb310f3323b2053ef359c
Dan Anstis <dan@bsod.co.nz>
2018-03-19 08:48:08 +1000

    Update formatting

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,9 +1,8 @@
 /* global angular */
 var app = angular.module("Plex-Sync", []);
 
-app.controller("indexImages", function($scope, $http){
-    $http.get("/api/shows")
-    .then(function(response) {
+app.controller("indexImages", function($scope, $http) {
+    $http.get("/api/shows").then(function(response) {
         $scope.records = response.data;
     });
 });
