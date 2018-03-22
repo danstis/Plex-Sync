0ebeaa30e83c8c28886f35ac11d98a70f73000cc
Dan Anstis <dan@bsod.co.nz>
2018-03-22 15:16:30 +1000

    Add tests for finding all episodes

--- plex/server_test.go
+++ plex/server_test.go
@@ -68,16 +68,16 @@ func TestSearchShow(t *testing.T) {
 	}
 
 	Convey("When searching for as existing show", t, func() {
-		result, err := SearchShow(host, "Cops")
+		result, err := SearchShow(host, "GoodShow")
 		if err != nil {
 			t.Errorf("unable to search show: %v", err)
 		}
 		expected := Show{
-			ID:           273,
-			Name:         "Cops",
-			EpisodeCount: 5,
-			Thumbnail:    "/library/metadata/273/thumb/1519355692",
-			Banner:       "/library/metadata/273/banner/1519355692",
+			ID:           123,
+			Name:         "GoodShow",
+			EpisodeCount: 2,
+			Thumbnail:    "/library/metadata/123/thumb/123456789",
+			Banner:       "/library/metadata/123/banner/123456789",
 		}
 
 		Convey("The correct show details should be returned", func() {
@@ -92,6 +92,46 @@ func TestSearchShow(t *testing.T) {
 		Convey("An empty show and an error should be returned", func() {
 			So(result, ShouldResemble, expected)
 			So(err, ShouldResemble, fmt.Errorf("no show found matching name %q", "BadShow"))
+		})
+	})
+}
+
+func TestGettingShowEpisodes(t *testing.T) {
+	ts := startTestServer()
+	defer ts.Close()
+	port, err := strconv.Atoi(strings.Split(ts.URL, ":")[2])
+	if err != nil {
+		t.Errorf("failed to start test server")
+	}
+	host := Host{
+		Name:     "TestServer",
+		Hostname: "127.0.0.1",
+		Port:     port,
+		Ssl:      false,
+	}
+
+	Convey("When searching for all show episodes", t, func() {
+		result, err := allEpisodes(host, 123)
+		if err != nil {
+			t.Errorf("unable to get show episodes: %v", err)
+		}
+		expected := []Episode{
+			Episode{ID: 128, Name: "Episode1", Episode: 1, Season: 1, ViewCount: 1, LastWatched: 1519792250},
+			Episode{ID: 125, Name: "Episode2", Episode: 2, Season: 1, ViewCount: 0, LastWatched: 0},
+		}
+
+		Convey("The correct episode details should be returned", func() {
+			So(result, ShouldResemble, expected)
+		})
+	})
+
+	Convey("When searching for a non existing show", t, func() {
+		result, err := allEpisodes(host, 000)
+		expected := []Episode{}
+
+		Convey("An empty show and an error should be returned", func() {
+			So(result, ShouldResemble, expected)
+			So(err, ShouldResemble, fmt.Errorf("error parsing xml response: %v", "EOF"))
 		})
 	})
 }
@@ -122,19 +162,38 @@ func tempfile(t *testing.T, name string, age int) os.FileInfo {
 func startTestServer() *httptest.Server {
 	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.EscapedPath() + "?" + r.URL.RawQuery {
-		case "/search?type=2&query=Cops":
+
+		case "/search?type=2&query=GoodShow":
 			w.Header().Set("Content-Type", "text/xml;charset=utf-8")
 			fmt.Fprintln(w, `<?xml version="1.0" encoding="UTF-8"?>
                 <MediaContainer size="1" identifier="com.plexapp.plugins.library" mediaTagPrefix="/system/bundle/media/flags/" mediaTagVersion="1518751453">
-                    <Directory allowSync="1" librarySectionID="2" librarySectionTitle="TV Shows" personal="1" ratingKey="273" key="/library/metadata/273/children" studio="Paramount Network" type="show" title="Cops" contentRating="TV-MA" summary="Description Blurb." index="1" rating="9.0" viewCount="5" lastViewedAt="1519811198" year="1989" thumb="/library/metadata/273/thumb/1519355692" art="/library/metadata/273/art/1519355692" banner="/library/metadata/273/banner/1519355692" theme="/library/metadata/273/theme/1519355692" duration="1500000" originallyAvailableAt="1989-03-01" leafCount="5" viewedLeafCount="5" childCount="1" addedAt="1519355149" updatedAt="1519355692">
+                    <Directory allowSync="1" librarySectionID="2" librarySectionTitle="TV Shows" personal="1" ratingKey="123" key="/library/metadata/123/children" studio="A Network" type="show" title="GoodShow" contentRating="TV-MA" summary="Description Blurb." index="1" rating="9.0" viewCount="5" lastViewedAt="1519811198" year="1989" thumb="/library/metadata/123/thumb/123456789" art="/library/metadata/123/art/123456789" banner="/library/metadata/123/banner/123456789" theme="/library/metadata/123/theme/123456789" duration="1500000" originallyAvailableAt="1989-03-01" leafCount="2" viewedLeafCount="2" childCount="1" addedAt="1519355149" updatedAt="123456789">
                         <Genre tag="Reality" />
                     </Directory>
                 </MediaContainer>`)
+
 		case "/search?type=2&query=BadShow":
 			w.Header().Set("Content-Type", "text/xml;charset=utf-8")
 			fmt.Fprintln(w, `<?xml version="1.0" encoding="UTF-8"?>
                 <MediaContainer size="0" identifier="com.plexapp.plugins.library" mediaTagPrefix="/system/bundle/media/flags/" mediaTagVersion="1518751453"></MediaContainer>`)
+
+		case "/library/metadata/123/allLeaves?":
+			w.Header().Set("Content-Type", "text/xml;charset=utf-8")
+			fmt.Fprintln(w, `<?xml version="1.0" encoding="UTF-8"?>
+                <MediaContainer size="2" allowSync="1" art="/library/metadata/123/art/123456789" banner="/library/metadata/123/banner/123456789" identifier="com.plexapp.plugins.library" key="123" librarySectionID="2" librarySectionTitle="TV Shows" mediaTagPrefix="/system/bundle/media/flags/" mediaTagVersion="1518751453" mixedParents="1" nocache="1" parentIndex="1" parentTitle="GoodShow" parentYear="1989" theme="/library/metadata/123/theme/123456789" title1="TV Shows" title2="GoodShow" viewGroup="episode" viewMode="65592">
+                    <Video ratingKey="128" key="/library/metadata/128" parentRatingKey="124" studio="A Network" type="episode" title="Episode1" parentKey="/library/metadata/124" grandparentTitle="GoodShow" parentTitle="Season 1" contentRating="TV-MA" summary="Episode summary." index="1" parentIndex="1" viewCount="1" lastViewedAt="1519792250" year="2018" thumb="/library/metadata/128/thumb/1519862671" art="/library/metadata/-1/art/1519355672" parentThumb="/library/metadata/124/thumb/123456789" grandparentThumb="/library/metadata/-1/thumb/1519355672" grandparentArt="/library/metadata/-1/art/1519355672" grandparentTheme="/library/metadata/-1/theme/1519355672" duration="1194894" originallyAvailableAt="2018-01-22" addedAt="1519355672" updatedAt="1519862671">
+                        <Media videoResolution="720" id="1001" duration="1194894" bitrate="4885" width="1280" height="720" aspectRatio="1.78" audioChannels="2" audioCodec="ac3" videoCodec="h264" container="mkv" videoFrameRate="60p" videoProfile="high">
+                            <Part id="1001" key="/library/parts/1001/1516684040/file.mkv" duration="1194894" file="/volume1/Media/Series/GoodShow/Season 1/GoodShow - S01E01 - Episode1.mkv" size="729690742" container="mkv" videoProfile="high" />
+                        </Media>
+                    </Video>
+                    <Video ratingKey="125" key="/library/metadata/125" parentRatingKey="124" studio="A Network" type="episode" title="Episode2" parentKey="/library/metadata/124" grandparentTitle="GoodShow" parentTitle="Season 1" contentRating="TV-MA" summary="Episode summary." index="2" parentIndex="1" year="2018" thumb="/library/metadata/125/thumb/1519862676" art="/library/metadata/-1/art/1519355149" parentThumb="/library/metadata/124/thumb/123456789" grandparentThumb="/library/metadata/-1/thumb/1519355149" grandparentArt="/library/metadata/-1/art/1519355149" grandparentTheme="/library/metadata/-1/theme/1519355149" duration="1215335" originallyAvailableAt="2018-01-29" addedAt="1519355149" updatedAt="1519862676">
+                        <Media videoResolution="1080" id="1002" duration="1215335" bitrate="3596" width="1920" height="1080" aspectRatio="1.78" audioChannels="2" audioCodec="aac" videoCodec="hevc" container="mkv" videoFrameRate="NTSC" audioProfile="lc" videoProfile="main">
+                            <Part id="1002" key="/library/parts/1002/1518352670/file.mkv" duration="1215335" file="/volume1/Media/Series/GoodShow/Season 1/GoodShow - S01E02 - Episode2.mkv" size="546330868" audioProfile="lc" container="mkv" videoProfile="main" />
+                        </Media>
+                    </Video>
+                </MediaContainer>`)
 		}
+
 	}))
 
 	return s
